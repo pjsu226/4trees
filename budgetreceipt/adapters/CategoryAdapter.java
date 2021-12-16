808b89a0782fbe3b550b1d8cb23302f18391cf39
d9810 <83438191+d9810@users.noreply.github.com>
2021-12-17 07:14:32 +0900

    LayoutInflater를 통한 뷰 구현.

---
+++ budgetreceipt/adapters/CategoryAdapter.java
@@ -0,0 +1,58 @@
+package com.example.budgetreceipt.adapters;
+
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.budgetreceipt.R;
+import com.example.budgetreceipt.models.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+
+    private List<Category> categoryList;
+
+    public CategoryAdapter(List<Category> categoryList) {
+        this.categoryList = categoryList;
+    }
+
+    @Override
+    public CategoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // inflating recycler item view
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_category_recycler, parent, false);
+
+        return new CategoryViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(CategoryViewHolder holder, int position) {
+        holder.textViewName.setText(categoryList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.v(CategoryAdapter.class.getSimpleName(),""+categoryList.size());
+        return categoryList.size();
+    }
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    /**
+     * ViewHolder class
+     */
+    public class CategoryViewHolder extends RecyclerView.ViewHolder {
+
+        public AppCompatTextView textViewName;
+
+        public CategoryViewHolder(View view) {
+            super(view);
+            textViewName = (AppCompatTextView) view.findViewById(R.id.textViewName);
+        }
+    }
+}
